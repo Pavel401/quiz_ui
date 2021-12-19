7424cc96c6ebf004da3d0a8a6fc4a5fec41bfe1e
Pavel401 <pavelalam401@gmail.com>
2021-12-19 13:47:56 +0530

    release

--- lib/components/card_with_time_tile.dart
+++ lib/components/card_with_time_tile.dart
@@ -17,67 +17,65 @@ class cards_time extends StatefulWidget {
 class _cards_timeState extends State<cards_time> {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Card(
-        elevation: 4,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-        child: Container(
-       height: 190,
-          width: 160,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              
-              Padding(
-                padding: const EdgeInsets.only(top: 50, bottom: 20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
+    return Card(
+      elevation: 4,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      child: Container(
+     height: 190,
+        width: 160,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            
+            Padding(
+              padding: const EdgeInsets.only(top: 50, bottom: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                   "hrs : mins",
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                 "hrs : mins",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: textcolor),
+                )
+              ],
+            ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      
+                      Text(
+                        widget.integer,
+                        style: TextStyle(
+                            color: HexColor("#"+widget.color),
+                            fontSize: 42,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 16),
+                  child: Text(
+                   widget.title,
                     style: TextStyle(
-                        fontSize: 18,
+                        fontSize: 25,
                         fontWeight: FontWeight.w500,
                         color: textcolor),
-                  )
-                ],
-              ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        
-                        Text(
-                          widget.integer,
-                          style: TextStyle(
-                              color: HexColor("#"+widget.color),
-                              fontSize: 42,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16),
-                    child: Text(
-                     widget.title,
-                      style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.w500,
-                          color: textcolor),
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
+                  ),
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
--- lib/components/cards.dart
+++ lib/components/cards.dart
@@ -24,44 +24,42 @@ class _cardsState extends State<cards> {
     return Card(
       elevation: 4,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      child: Expanded(
-        child: Container(
-          height: 190,
-          width: 160,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 65, bottom: 30),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      widget.integer,
-                      style: TextStyle(
-                          color: HexColor("#" + widget.color),
-                          fontSize: 42,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              ),
-              Row(
+      child: Container(
+        height: 190,
+        width: 160,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 65, bottom: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 5, bottom: 2),
-                    child: Text(
-                      widget.title,
-                      style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.w500,
-                          color: textcolor),
-                    ),
-                  )
+                  Text(
+                    widget.integer,
+                    style: TextStyle(
+                        color: HexColor("#" + widget.color),
+                        fontSize: 42,
+                        fontWeight: FontWeight.bold),
+                  ),
                 ],
-              )
-            ],
-          ),
+              ),
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 5, bottom: 2),
+                  child: Text(
+                    widget.title,
+                    style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w500,
+                        color: textcolor),
+                  ),
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
